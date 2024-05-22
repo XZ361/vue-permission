b3b6d2085c383b967380c85e4b2dc026ab953493
XZ361 <2457307263@qq.com>
2024-05-22 09:53:07 +0800

    初始化项目

---
+++ src/pages/login/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="login-wrap" class="clearfix">
+    <div class="title">登录页</div>
+    <div id="login">
+      <div class="login-account">
+        <span>账号：</span>
+        <input
+          type="text"
+          v-model.trim="account"
+          name="account"
+          placeholder="账号"
+        />
+      </div>
+      <div class="login-password">
+        <span>密码：</span>
+        <input
+          type="password"
+          v-model.trim="password"
+          name="password"
+          placeholder="密码"
+          @keyup.enter="password"
+        />
+      </div>
+      <p class="login-btn">
+        <button id="loginBtn" @click="login">登录</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      account: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // 网络请求
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,64 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../pages/login";
+import Home from "../pages/home";
+import NotFound from "../pages/errorPage/404";
+import Forbidden from "../pages/errorPage/403";
+import Layout from "../pages/layout";
 
-Vue.use(VueRouter)
-
+Vue.use(VueRouter);
+// 初始化路由
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+];
+
+// 根据用户权限的不同，所能看到的页面和可操作性也不同
+// admin->可看到所有页面
+// vip->属于vip的权限
+// 准备动态加载的路由
+
+export const DynamicRoutes = [
+  {
+    path: "",
+    component: Layout,
+    name: "container",
+    redirect: "home",
+    meta: {
+      requiresAuth: true,
+      name: "首页",
+    },
+    children: [
+      {
+        path: "home",
+        component: Home,
+        name: "home",
+        meat: {
+          // 用来匹配规则
+          name: "首页",
+          icon: "icon-name",
+        },
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
+    path: "/403",
+    component: Forbidden,
+    name: "403",
+  },
+  {
+    path: "*",
+    component: NotFound,
+    name: "404",
+  },
+];
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
